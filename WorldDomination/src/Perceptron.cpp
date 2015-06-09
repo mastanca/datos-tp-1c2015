792589bab1c64a676f54613939106f7c0d84f72f
mastanca <mastanca14@gmail.com>
2015-06-09 00:43:25 -0300

    Agregado macro de Adrian para el manejo de la matriz Vean ejemplo de uso en el codigo

--- WorldDomination/src/Perceptron.cpp
+++ WorldDomination/src/Perceptron.cpp
@@ -13,6 +13,7 @@
 using namespace std;
 
 #define CANTIDAD 25000
+#define index(fila, columna) 25000*fila + columna
 
 void Perceptron::ejecutar (){
 	Parseador parseador("data/train_data_limpia.csv");
@@ -23,9 +24,20 @@ void Perceptron::ejecutar (){
 	myFile.open("data/MatrizSimetricaBinaria-0-25000.dat", ios::binary | ios::in | ios::ate);
 	unsigned int size = myFile.tellg();
 	myFile.seekg(0, ios::beg);
-	float* buffer = new float [size*2];
-	myFile.read((char *)buffer, sizeof(float)*size);
-	delete []buffer;
+	float *matriz = new float [size*2];
+	myFile.read((char *)matriz, sizeof(float)*size);
+
+	/* EJEMPLO USO DE LA MATRIZ
+	cout << matriz[index(0,0)] << endl;
+	cout << matriz[index(5000,5000)] << endl;
+	cout << matriz[index(1487,23145)] << endl;
+	cout << matriz[index(23145,1487)] << endl;
+	*/
+	vector<int> vectorErrores;
+	int factorDeAprendizaje = 1;
+
+
+	delete []matriz;
 	myFile.close();
 
 }
