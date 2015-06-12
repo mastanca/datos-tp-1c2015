f1780c6cdbd5daa36a59e1402ca462af9ea704c6
horaciomk <hmk142@hotmail.com>
2015-06-12 00:00:34 -0300

    matriz

--- WorldDomination/src/Perceptron.cpp
+++ WorldDomination/src/Perceptron.cpp
@@ -4,40 +4,66 @@
  *  Created on: Jun 6, 2015
  *      Author: mastanca
  */
-
 #include <vector>
 #include "Parseador.h"
 #include "Perceptron.h"
 #include <fstream>
 #include <iostream>
+
 using namespace std;
 
 #define CANTIDAD 25000
-#define index(fila, columna) 25000*fila + columna
-
 void Perceptron::ejecutar (){
 	Parseador parseador("data/train_data_limpia.csv");
 	vector<label> vectorLabels;
 	vectorLabels = parseador.getLabels(CANTIDAD);
 
-	fstream myFile;
-	myFile.open("data/MatrizSimetricaBinaria-0-25000.dat", ios::binary | ios::in | ios::ate);
-	unsigned int size = myFile.tellg();
-	myFile.seekg(0, ios::beg);
-	float *matriz = new float [size*2];
-	myFile.read((char *)matriz, sizeof(float)*size);
+	std::ifstream file("data/MatrizSimetricaBinaria-0-25000.dat", std::ifstream::ate | std::ifstream::binary);
+  unsigned int size = file.tellg();
+  file.seekg(0, file.beg);
+	vector<vector<float> > matriz(CANTIDAD, vector<float>(CANTIDAD));
+	for (int i = 0;i<CANTIDAD;i++)
+	  for (int j=0;j < CANTIDAD;j++)
+	     file.read((char*)(&matriz[i][j]), sizeof(float));
 
-	/* EJEMPLO USO DE LA MATRIZ
-	cout << matriz[index(0,0)] << endl;
-	cout << matriz[index(5000,5000)] << endl;
-	cout << matriz[index(1487,23145)] << endl;
-	cout << matriz[index(23145,1487)] << endl;
-	*/
+	cout << matriz[0][0] << endl;
+	cout << matriz[1][1] << endl;
+	cout << matriz[100][0] << endl;
+	cout << matriz[100][100] << endl;
+
 	vector<int> vectorErrores;
 	int factorDeAprendizaje = 1;
+	file.close();
+}
+/*
+/////////////////////////////// Como estaba antes:
+
+#define index(fila, columna) 25000*fila + columna
+
+void Perceptron::ejecutar (){
+  Parseador parseador("data/train_data_limpia.csv");
+  vector<label> vectorLabels;
+  vectorLabels = parseador.getLabels(CANTIDAD);
+
+  fstream myFile;
+  myFile.open("data/MatrizSimetricaBinaria-0-25000.dat", ios::binary | ios::in | ios::ate);
+  unsigned int size = myFile.tellg();
+  myFile.seekg(0, ios::beg);
+  float *matriz = new float [size*2];
+  myFile.read((char *)matriz, sizeof(float)*size);
+
+  /* EJEMPLO USO DE LA MATRIZ
+  cout << matriz[index(0,0)] << endl;
+  cout << matriz[index(5000,5000)] << endl;
+  cout << matriz[index(1487,23145)] << endl;
+  cout << matriz[index(23145,1487)] << endl;
+
+  vector<int> vectorErrores;
+  int factorDeAprendizaje = 1;
 
 
-	delete []matriz;
-	myFile.close();
+  delete []matriz;
+  myFile.close();
 
 }
+*/
