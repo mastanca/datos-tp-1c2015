8ec14deb2defae15eaa36e0feb3cc41b7a072fb4
horaciomk <hmk142@hotmail.com>
2015-06-16 14:13:27 -0300

    resultados perceptron

--- WorldDomination/src/Perceptron.cpp
+++ WorldDomination/src/Perceptron.cpp
@@ -7,6 +7,7 @@
 #include <vector>
 #include "Parseador.h"
 #include "Perceptron.h"
+#include "NCD.h"
 #include <fstream>
 #include <iostream>
 #include <math.h>
@@ -33,17 +34,16 @@ void Perceptron::ejecutar() {
       file.read((char*) (&matriz[i][j]), sizeof(float));
 
   for (int i = 0; i < 200; i++)
-      for (int j = 0; j < 200; j++)
-        cout << (float)matriz[i][j] << endl;
+    for (int j = 0; j < 200; j++)
+      cout << (float) matriz[i][j] << endl;
 
   /*for (int i = 0; i < 5; i++)
-      std::cout << matriz[i][i] << endl;*/
+   std::cout << matriz[i][i] << endl;*/
 
   /*for (int i = 0; i < 25000; i++)
-    matriz[i][i] = 0;*/
+   matriz[i][i] = 0;*/
 
   //entrenar:
-
   std::vector<float> pesos(CANTIDAD_REVIEWS_ENTRENAMIENTO, 0.0);
 
   int cantidadErrores;
@@ -55,7 +55,7 @@ void Perceptron::ejecutar() {
       resultado = 0;
       for (int j = 0; j < CANTIDAD_REVIEWS_ENTRENAMIENTO; ++j) {
 
-    	//double res = 1 - 1/(1 + exp(-(204.6720082*matriz[i][j] - 193.4150478)));
+        //double res = 1 - 1/(1 + exp(-(204.6720082*matriz[i][j] - 193.4150478)));
         double res = 1 - matriz[i][j];
         if (reviewsEntrenamiento[j].sentimiento == 1)
           resultado = resultado + (res * pesos[j]);
@@ -82,10 +82,38 @@ void Perceptron::ejecutar() {
       break;
   }
 
-  for (int i = 0; i < CANTIDAD_REVIEWS_ENTRENAMIENTO; i++){
+  for (int i = 0; i < CANTIDAD_REVIEWS_ENTRENAMIENTO; i++) {
     cout << pesos[i] << " ";
   }
   cout << "Fin entrenamiento perceptron" << endl;
+
+  //evaluar:
+  NCD ncd;
+  Parseador parseadorTest("data/test_data_limpia.csv");
+  vector<review> reviewsTest = parseadorTest.getTestReviews();
+
+  for (int i = 0; i < 25000; i++) {
+    double resultado = 0;
+    for (int j = 0; j < CANTIDAD_REVIEWS_ENTRENAMIENTO; ++j) {
+      double kernel = 1
+          - ncd.calcular(reviewsTest[i].texto, reviewsEntrenamiento[j].texto);
+
+      if (reviewsEntrenamiento[j].sentimiento > 0)
+        resultado = resultado + kernel * pesos[j];
+      else
+        resultado = resultado - kernel * pesos[j];
+    }
+
+    if (resultado > 0)
+      reviewsTest[i].sentimiento = 1;
+    else
+      reviewsTest[i].sentimiento = 0;
+
+    cout << i << endl;
+  }
+
+  parseadorTest.escribir_resultados(reviewsTest,
+                                    "data/resultadosPerceptron.csv");
 
 }
 /*
